55aec5f1390adaa9594211b28c05fe1bcd3bffc7
Ian <105128049+minijae011030@users.noreply.github.com>
2025-10-07 04:42:29 +0900

    🟢 Test(Counter): 테스트 통과를 위해 증가 버튼 클릭 로직 추가

--- src/features/Counter/ui/Counter.tsx
+++ src/features/Counter/ui/Counter.tsx
@@ -3,5 +3,10 @@ import { useState } from "react";
 export default function Counter({ initial = 0 }) {
   const [count, setCount] = useState(0);
 
-  return <div>Count: 0</div>;
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>증가</button>
+    </div>
+  );
 }

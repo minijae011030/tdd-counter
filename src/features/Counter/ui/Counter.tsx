a8b41ee77780425bc9111fddb6496e72f8079435
Ian <105128049+minijae011030@users.noreply.github.com>
2025-10-07 04:49:36 +0900

    ♻️  Refactor(Counter): 카운터 값을 initial 값으로 수정

--- src/features/Counter/ui/Counter.tsx
+++ src/features/Counter/ui/Counter.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function Counter({ initial = 0 }) {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(initial);
 
   return (
     <div>
